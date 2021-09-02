8d010ae7f02fb522aa96c086c0185612a25038ff
Grace Gardner <graceirisgardner@gmail.com>
2021-09-01 20:28:34 -0600

    Add customer

--- object-examples.js
+++ object-examples.js
@@ -1,3 +1,5 @@
 var car = { model:"Corrolla", color:"white", year:"1996"};
 
 var animal = {type:"dog", age:8, breed:"Aussie"};
+
+var customer = {loyaltyNumber:12345, customerName:"Sam", numberOfVisits:22};
